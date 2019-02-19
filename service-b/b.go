053697d7861049b9f03eb550387696e0b8830452
Kim Inseo <sano307@naver.com>
2019-02-19 10:49:04 +0900

    Create two service directory

---
+++ service-b/b.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hc", hc)
+
+	log.Fatal(http.ListenAndServe(":4000", nil))
+}
+
+func hc(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "This is service b")
+}
